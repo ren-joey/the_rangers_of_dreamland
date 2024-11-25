14059b8bf7489dd2548419483019c9fc21e14a2d
ren-joey <tsuyiren@gmail.com>
2024-11-25 20:36:30 +0800

    feat: 🎸 initialization the project

---
+++ src/main/java/com/trod/TrodApplication/TrodApplication.java
@@ -0,0 +1,13 @@
+package com.trod.TrodApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrodApplication.class, args);
+	}
+
+}
