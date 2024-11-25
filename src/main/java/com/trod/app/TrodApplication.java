d6109c813eff67964e5cacff58ae085e5e473a1e
ren-joey <tsuyiren@gmail.com>
2024-11-25 21:39:25 +0800

    feat: 🎸 finished basic structure

--- src/main/java/com/trod/app/TrodApplication.java
+++ src/main/java/com/trod/app/TrodApplication.java
@@ -1,9 +1,14 @@
-package com.trod.TrodApplication;
+package com.trod.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@ComponentScan("com.trod")
 public class TrodApplication {
 
 	public static void main(String[] args) {
--- src/test/java/com/trod/app/TrodApplicationTests.java
+++ src/test/java/com/trod/app/TrodApplicationTests.java
@@ -1,4 +1,4 @@
-package com.trod.TrodApplication;
+package com.trod.app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
