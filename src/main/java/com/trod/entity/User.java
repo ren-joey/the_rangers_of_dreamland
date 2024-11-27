ddbf8779d6f08d372b7210e57f4291d34b120259
JOEY.REN 任祖頤 <JOEY.REN@deltaww.com>
2024-11-27 10:37:00 +0800

    perf: ⚡️ adapt @Getter, @Setter

--- src/main/java/com/trod/entity/User.java
+++ src/main/java/com/trod/entity/User.java
@@ -1,7 +1,11 @@
 package com.trod.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 public class User {
     @Id
@@ -16,37 +20,4 @@ public class User {
 
     @Column(nullable = false, unique = true)
     private String email;
-
-    // Getter and Setter methods
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
