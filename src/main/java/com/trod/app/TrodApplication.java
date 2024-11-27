f70b52e6abf8aae3423ccca6052097cc6e0d4a10
ren-joey <tsuyiren@gmail.com>
2024-11-27 09:32:38 +0800

    feat: 🎸 add environmental setting processor

--- src/main/java/com/trod/app/TrodApplication.java
+++ src/main/java/com/trod/app/TrodApplication.java
@@ -1,25 +1,21 @@
 package com.trod.app;
 
-import io.github.cdimascio.dotenv.Dotenv;
+import com.trod.post.processor.DotenvPostProcessor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 @ComponentScan("com.trod")
 @MapperScan("com.trod.mapper")
 @EntityScan("com.trod.entity")
-public class TrodApplication {
+public class TrodApplication  {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+		DotenvPostProcessor.loadEnv();
 
 		SpringApplication.run(TrodApplication.class, args);
 	}
-
 }
