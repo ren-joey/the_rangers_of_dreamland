89677ff13a2725fc314ce82fea67f13fc3fe1173
ren-joey <tsuyiren@gmail.com>
2025-01-16 16:27:15 +0800

    chore: remove unused imports

--- src/main/java/com/trod/mapper/CardMapper.java
+++ src/main/java/com/trod/mapper/CardMapper.java
@@ -1,7 +1,6 @@
 package com.trod.mapper;
 
 import com.trod.entity.Monster;
-import org.apache.ibatis.annotations.Insert;
 
 public interface CardMapper {
     void insertMonsterCard(Monster monster);
